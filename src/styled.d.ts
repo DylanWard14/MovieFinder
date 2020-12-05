b1dee2faee4d784c14c1548ff856c807bd81a9f0
DylanWard14 <dylan.ward@iinet.net.au>
2020-12-05 15:48:41 +1000

    Updated theme

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -14,6 +14,7 @@ declare module 'styled-components' {
             main: string;
             secondary: string;
             error: string;
+            blue: string;
         };
         typography: {
             heading: Typography;
