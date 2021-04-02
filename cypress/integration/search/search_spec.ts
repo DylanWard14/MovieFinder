53fcf45eabf04e467cd4648a6054c0661bf80d6a
DylanWard14 <dylan.ward@iinet.net.au>
2021-04-02 16:08:21 +1000

    Intercepting the search request in the search_spec cypress test and faking the search response. Updated search_spec test to confirm correct amount of children are being displayed.

--- cypress/integration/search/search_spec.ts
+++ cypress/integration/search/search_spec.ts
@@ -5,6 +5,18 @@
 // ***************************************************************
 
 describe('Search', () => {
+    beforeEach(() => {
+        const API_KEY = Cypress.env('REACT_API_KEY');
+
+        // # Intercept the search request and fake the response
+        cy.intercept(
+            `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=Harry Potter and the&page=1&include_adult=false`,
+            {
+                fixture: 'search',
+            },
+        );
+    });
+
     it('Can search and select movie', () => {
         // # Visit the home page
         cy.visit('/');
@@ -13,25 +25,29 @@ describe('Search', () => {
         cy.get('#search').should('have.value', '');
 
         // # Get the search input field, type a value
-        cy.get('#search').type('Harry Potter and the Chamber of Secrets');
+        cy.get('#search').type('Harry Potter and the');
 
         // * The search input should contain the value
-        cy.get('#search').should('have.value', 'Harry Potter and the Chamber of Secrets');
+        cy.get('#search').should('have.value', 'Harry Potter and the');
 
         // # Get the search form and submit it
         cy.get('#search_form').submit();
 
+        const resultsList = cy.get('#search_results');
+
         // * Get the search results list and check it has children
-        cy.get('#search_results')
-            .children()
-            .should('not.be.null');
+        resultsList.children().should('not.be.null');
+
+        // * Get the search results list and check it has the correct amount of children
+        resultsList.children().should('have.length', 13);
+
+        // * Get the first result in the list and ensure that it has the correct value
+        resultsList.first().should('have.text', "Harry Potter and the Philosopher's Stone");
 
         // # Get the first child in the search results list and click it
-        cy.get('#search_results')
-            .first()
-            .click();
+        resultsList.first().click();
 
         // * Check that the url now includes the movies ID
-        cy.url().should('include', '672'); // This is the ID for Harry Potter and the Chamber of Secrets
+        cy.url().should('include', '671');
     });
 });
